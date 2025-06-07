980e5d1469919699aa928591f7d88d544eca05ab
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-06-07 22:41:37 +0300

    added new icons

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
-
+import { Button } from "@/components/ui/button";
+import { PlaneTakeoff  } from 'lucide-react';
 
 
 
 
 export default function Search() {
   return (
-    <section className="flex items-start justify-center flex-col gap-4 max-h-screen max-w-6xl mx-auto w-full mb-8">
-      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-slate-800">Find a Traveler who's ready to go.</h1>
+    <section className="flex items-start justify-center flex-col gap-4 max-h-screen max-w-6xl mx-auto w-full mb-8 p-2">
+      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-slate-900">Search for travelers</h1>
 
-      <form className="flex items-start justify-center gap-3">
+      <form className="flex items-center justify-center gap-4">
         <div className="flex w-full max-w-sm items-center gap-3">
           <Label htmlFor="from">From:</Label>
           <Input type="text" id="from" placeholder="Country" />
         </div>
-
-        <div className="flex w-full max-w-sm items-center gap-3">
+         <PlaneTakeoff className="size-16"/>
+        <div className="flex w-full max-w-sm items-center gap-4">
           <Label htmlFor="to">To:</Label>
           <Input type="text" id="to" placeholder="Country" />
         </div>
-        <button className="bg-orange-500 px-4 py-2 rounded-md text-white hover:bg-orange-400 hover:shadow-lg">Find</button>
+        <Button className="px-5 py-2">Find</Button>
       </form>
     </section>
   );
